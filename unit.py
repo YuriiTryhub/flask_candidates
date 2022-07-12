49ed7d9c7e7d84acf55bd948afe340a5ec4f9eff
Yurii Trihub <yuriitrihub@gmail.com>
2022-07-12 18:28:59 +0300

    get_all(candidates) возвращает всех кандиатов

--- unit.py
+++ unit.py
@@ -5,18 +5,21 @@ def load_candidates():
     """которая загрузит данные из файла"""
     with open('candidates.json', mode='r', encoding='utf-8') as f:
         candidates = json.load(f)
-    print(candidates)
+    return candidates
 
 
-def get_all():
-    '''которая покажет всех кандидатов'''
-    pass
+def get_all(candidates):
+    """которая покажет всех кандидатов"""
+    for i in candidates:
+        candidates_list = i['name']
+        return candidates_list
+
 
 
 def get_by_pk(pk):
-    '''которая вернет кандидата по pk'''
+    """которая вернет кандидата по pk"""
     pass
 
 def get_by_skill(skill_name):
-    '''которая вернет кандидатов по навыку'''
+    """которая вернет кандидатов по навыку"""
     pass
